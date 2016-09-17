ac82875b8a340af04bf0347825fb39b1490e47cd
zaboople <troymotte@yahoo.com>
2016-09-17 09:39:40 -0500

    Logging utilities

---
+++ java/org/tmotte/choogle/pagecrawl/SiteCrawlerDebug.java
@@ -0,0 +1,92 @@
+package org.tmotte.choogle.pagecrawl;
+import java.net.URI;
+import org.tmotte.common.text.Outlog;
+
+class SiteCrawlerDebug {
+
+  Outlog log;
+  String sitehost;
+
+  void doHead(URI uri) {
+    if (log.is(2))
+      log.lf().date().add(sitehost).add(" HEAD: ").add(uri).lf();
+  }
+
+  void headers(
+      URI currentURI,
+      int statusCode,
+      String contentType,
+      String eTag,
+      String lastModified,
+      boolean closed,
+      boolean redirected,
+      String locationHeader
+    ){
+    log.add("  ").add(sitehost)
+      .add(" RESPONSE")
+      .add(" STATUS: ").add(statusCode)
+      .add(" CONTENT TYPE: ").add(contentType);
+    if (eTag!=null)
+      log.add(" ETAG: ").add(eTag);
+    if (lastModified !=null)
+      log.add(" LAST MODIFIED: ").add(lastModified);
+    if (closed)
+      log.lf()
+        .add("  ").add(sitehost).add(" CLOSED");
+    if (redirected)
+      log.lf()
+        .add("  ").add(sitehost).add(" REDIRECT: ").add(locationHeader);
+    log.lf();
+    if (log.is(2)) log.add("  ");
+  }
+
+  void pageBody(String s) {
+    if (log.is(4))
+      log.lf().add(">>>").add(s).add("<<<").lf();
+    else
+      log.add(".");
+  }
+
+  void pageComplete(
+      URI currentURI, int count, int pageSize, String title
+    ) {
+    if (log.is(2)) log.lf().add("  ");
+    else log.date();
+    log.add(sitehost).add(" COMPLETE #").add(count)
+      .add(" SIZE: ").add(pageSize / 1024).add("K")
+      .add(" URI: ").add(currentURI)
+      .add(" TITLE: ").add(title)
+      .lf()
+      ;
+  }
+
+  void linkProcessing(
+      int count, int templinks, int scheduled, int elsewhere
+    ) {
+    if (log.is(2))
+      log
+        .add("  ").add(sitehost)
+        .add(" RESPONSE COUNT: ").add(count);
+    if (log.is(3))
+      log
+        .add(" LINKS FOUND: ").add(templinks)
+        .add(" SCHEDULED: ").add(scheduled)
+        .add(" ELSEWHERE: ").add(elsewhere);
+    if (log.is(2))
+      log.lf();
+  }
+
+  void closing() {
+    log.add(sitehost).add(" CLOSING").lf();
+  }
+
+  void redirecting(URI newURI) {
+    log.date().add(sitehost).add(" REDIRECTING SITE TO ").add(newURI).lf();
+  }
+
+  void siteComplete(int count) {
+    log.date()
+      .add(sitehost).add(" ALL LINKS READ, CLOSING, COUNT: ").add(count)
+      .lf();
+  }
+}
---
+++ java/org/tmotte/common/text/Outlog.java
@@ -0,0 +1,68 @@
+package org.tmotte.common.text;
+
+public final class Outlog {
+
+  private int level=0;
+  private String dateFormat="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS%1$tz ";
+
+  public Outlog setLevel(int level) {
+    this.level=level;
+    return this;
+  }
+  public int getLevel() {
+    return level;
+  }
+  public boolean is(int level) {
+    return level <= this.level;
+  }
+
+  /**
+   * @param fmt Can be null, in which case date print is repressed
+   */
+  public Outlog setDateFormat(String fmt) {
+    this.dateFormat=fmt;
+    return this;
+  }
+
+
+  public Outlog date() {
+    if (dateFormat!=null)
+      System.out.append(
+        String.format(dateFormat, new java.util.Date())
+      );
+    return this;
+  }
+  public Outlog lf() {
+    System.out.append("\n");
+    return this;
+  }
+  public Outlog add(String s) {
+    System.out.append(s);
+    return this;
+  }
+  public Outlog add(Object o) {
+    System.out.append(o.toString());
+    return this;
+  }
+  public Outlog add(int i) {
+    System.out.append(String.valueOf(i));
+    return this;
+  }
+  public Outlog add(long i) {
+    System.out.append(String.valueOf(i));
+    return this;
+  }
+  public Outlog add(boolean i) {
+    System.out.append(String.valueOf(i));
+    return this;
+  }
+  public Outlog add(double d) {
+    System.out.append(String.valueOf(d));
+    return this;
+  }
+  public Outlog add(char c) {
+    System.out.append(c);
+    return this;
+  }
+
+}
