7d891618c5f42f66df220ce142ad2a63c5173256
zaboople <troymotte@yahoo.com>
2016-10-15 15:25:08 -0500

    Comments

--- lib/db/start.sh
+++ lib/db/start.sh
@@ -1,3 +1,5 @@
+# You should be able to run this repeatedly without shutting down.
 docker-machine start dev
 eval $(docker-machine env dev)
 docker-compose -f $(dirname $0)/docker-compose-postgres.yml up -d
+docker ps
